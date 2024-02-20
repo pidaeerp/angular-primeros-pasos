e5bcab7b454475a9191836d5a9640a3c2bd38d07
Oscar R. Vásquez Alfaro <pidaesisfocom@outlook.com>
2024-02-20 11:15:32 -0500

    Primer Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CounterModule,
+    HeroesModule,
+    DbzModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
